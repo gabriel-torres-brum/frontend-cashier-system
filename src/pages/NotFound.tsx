cfd7cccc434298cc67377e062d13998f226de2da
Gabriel Torres <gabriel.torres@oliveiratrust.com.br>
2023-03-08 14:11:59 -0300

    feat: adding notfound page and route

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,8 @@
+export const NotFound = () => {
+    return (
+        <div className="flex items-center flex-col gap-4 justify-center h-screen">
+            <h1 className="text-8xl font-bold tracking-wide text-primary-400">404</h1>
+            <h4 className="text-2xl">Página não encontrada!</h4>
+        </div>
+    )
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,12 @@
 import { createBrowserRouter } from "react-router-dom"
 import { Dashboard } from "./pages/Dashboard"
+import { NotFound } from "./pages/NotFound"
 
 export const routes = createBrowserRouter([
+    {
+        path: "*",
+        element: <NotFound />
+    },
     {
         path: "/",
         element: <Dashboard />,
